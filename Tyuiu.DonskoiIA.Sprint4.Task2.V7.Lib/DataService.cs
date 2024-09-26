5e2c52aa99e2dd83eb2b1da93c135d4b71a526b3
IYI4k <IYI4k@yandex.ru>
2024-09-26 21:58:30 +0500

    Task2

---
+++ Tyuiu.DonskoiIA.Sprint4.Task2.V7.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.DonskoiIA.Sprint4.Task2.V7.Lib
+{
+    public class DataService : ISprint4Task2V7
+    {
+        public int Calculate(int[] array)
+        {
+            int ans = 0;
+            foreach (int i in array)
+            {
+                if (i % 2 == 0) ans += i;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ Tyuiu.DonskoiIA.Sprint4.Task2.V7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DonskoiIA.Sprint4.Task2.V7.Lib;
+
+namespace Tyuiu.DonskoiIA.Sprint4.Task2.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Донской И. А. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Донской Иван Андреевич | АСОиУб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая получает от пользователя длину массива,     *");
+            Console.WriteLine("* затем она заполнит его рандомными целыми числами, а потом выведет на    *");
+            Console.WriteLine("* экран сумму чётных чисел из этого массива.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите длину массива:");
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            Random ran = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = ran.Next(4 , 10);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(array));
+
+            Console.ReadLine();
+        }
+    }
+}
